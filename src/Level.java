461d58d05eb0ea2326ff1e1b1451fcfa95e8b545
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2021-04-06 07:09:25 -0700

    add support for items

--- src/Level.java
+++ src/Level.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class Level {
     private HashMap<String, Room> rooms;
@@ -38,10 +37,46 @@ public class Level {
     public class Room {
         private String name;
         private ArrayList<Room> neighbors;
+        private ArrayList<Item> inventory;
 
         private Room(String name) {
             neighbors = new ArrayList<>();
             this.name = name;
+            inventory = new ArrayList<>();
+        }
+
+        public void addItem(Item i) {
+            inventory.add(i);
+        }
+
+        public Item removeItem(String name) {
+            name = name.trim();
+            int index = getIndexForItem(name);
+            if (index == -1) return null;
+            return inventory.remove(index);
+        }
+
+        private int getIndexForItem(String name) {
+            for (int index = 0; index < inventory.size(); index++) {
+                if (inventory.get(index).getName().equals(name)) {
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        public boolean contains(String itemName) {
+            return (getIndexForItem(itemName) != -1);
+        }
+
+        public String getInvetoryString() {
+            if (inventory.size() == 0) return "Room has no items.";
+
+            String out = "";
+            for (Item i : inventory) {
+                out += i.getName() + ", ";
+            }
+            return out.substring(0, out.length() - 2);
         }
 
         public String getName(){
@@ -76,5 +111,6 @@ public class Level {
         public String toString() {
             return this.name;
         }
+
     }
 }
